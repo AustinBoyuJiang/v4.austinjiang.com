7e83abf6c2707c5d07a637095ad76647c51a9c83
Austin Jiang <austinjiangboyu@gmail.com>
2025-09-16 02:13:55 -0400

    Update

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -103,6 +103,14 @@ const BlogSection = ({ blogPosts, onPostClick }) => {
                       </div>
                     )}
                     <div className="blog-post-tags">
+                      {post.author && (
+                        <span className="blog-section-author">
+                          <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
+                          </svg>
+                          {post.author}
+                        </span>
+                      )}
                       {post.tags.map(tag => (
                         <span key={tag} className="blog-tag">{tag}</span>
                       ))}
